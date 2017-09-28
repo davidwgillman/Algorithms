8ecfbf9688e4de8619b20089fdd4a5fa641edad1
sunwooha <sunwoo.ha15@ncf.edu>
2017-09-28 13:45:04 -0400

    finished writing code

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -11,12 +11,30 @@ public final class KnuthShuffle {
      * @param  a the array to shuffle
      */
 	public static void shuffle(Object[] a) {
-		// as a demo, this generates a random number from 0 to 9
-		int i = StdRandom.uniform(0,10); 
-		// implement this
+		for(int n = 0; n < a.length; n++){
+			int i = StdRandom.uniform(0, a.length + 1); 
+			int swappedElem = a[i];
+			a[i] = a[n];
+			a[n] = swappedElem;
+		}
 	}
 
 	public static void main(String[] args) {
-		// implement this
+		int inputArraySize = args[0];
+		String[] a = new int[inputArraySize];
+
+		for(int i = 0; i < inputArraySize; i++){
+			a[i] = i;
+		}
+
+		Stopwatch timer = new Stopwatch();
+		KnuthShuffle.shuffle(a);
+		double time = timer.elapsedTime();
+		System.out.println(time);
+
+		for(int n = 0; n < inputArraySize; n++){
+			System.out.print(a[n]+ ", ");
+		}
+
 	}
 }
