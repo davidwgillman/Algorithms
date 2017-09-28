348abb6b209298ff3abb05b6a03539b81955a444
brigham4 <eric.brigham15@ncf.edu>
2017-09-28 13:43:34 -0400

    commit 1

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -11,12 +11,35 @@ public final class KnuthShuffle {
      * @param  a the array to shuffle
      */
 	public static void shuffle(Object[] a) {
-		// as a demo, this generates a random number from 0 to 9
-		int i = StdRandom.uniform(0,10); 
+		// as a demo, this generates a random number from 0 to to the the length of the arry
+		//this is the length of the array
+		int length = a.length;
+		int i = StdRandom.uniform(0, length); 
 		// implement this
+		for(n = 0; n < length; n++){
+			int randomSpot = StdRandom.uniform(0, length + 1);
+			Object assignSpot = a[randomSpot];
+			a[n] = a[randomSpot];
+			a[n] = assignSpot;
+		}
 	}
 
 	public static void main(String[] args) {
 		// implement this
+		int numElem = args[0];
+		int [] a = new int[numElem];
+		for(n =0; n < numElem; n++){
+			a[n] =n;
+		}
+
+		//printing time
+		Stopwatch timer = new Stopwatch();
+		KnuthShuffle.shuffle(a);
+		double time = timer.elapsedTime();
+		System.out.println(time);
+		//printing array
+		for(int n =0; n < a.length; n++){
+			System.out.println(a[n] + ", ");
+		}        
 	}
 }
