9fdf1194f2b99c939b2f18e07bd7cb65b6bdf4be
dianatarazi <diana.tarazi15@ncf.edu>
2017-09-28 15:09:26 -0400

    Finished shuffle function

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -12,11 +12,36 @@ public final class KnuthShuffle {
      */
 	public static void shuffle(Object[] a) {
 		// as a demo, this generates a random number from 0 to 9
-		int i = StdRandom.uniform(0,10); 
+		//int i = StdRandom.uniform(0,10); 
 		// implement this
+		validateNotNull(a);
+		int n = a.length;
+		for  (int i = 0; i < n; i++) {
+			int r = StdRandom.uniform(0, i+1);
+			Object temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+
+		}
+		
+		
 	}
 
 	public static void main(String[] args) {
 		// implement this
+		for (int i = 0; i < 10000; i++) {
+			a[i] = i;
+		}
+		Stopwatch timer = new Stopwatch();
+		
+		KnuthShuffle.shuffle();
+
+		double timer1 = timer.elapsedTime();
+        StdOut.printf("(%.2f seconds)\n", timer1);
+
+
+
+
+
 	}
 }
