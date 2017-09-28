16a61dafad4ac103c7512fee32f51beb2f70b1a8
davidwgillman <dgillman@ncf.edu>
2017-09-27 22:30:00 -0400

    Empty files.

---
+++ Workshop5/KnuthShuffle.java
@@ -0,0 +1,22 @@
+/* Shuffles an array of objects */
+
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.StdRandom;
+
+public final class KnuthShuffle {
+
+    /**
+     * Rearranges the elements of the specified array in uniformly random order.
+     *
+     * @param  a the array to shuffle
+     */
+	public static void shuffle(Object[] a) {
+		// as a demo, this generates a random number from 0 to 9
+		int i = StdRandom.uniform(0,10); 
+		// implement this
+	}
+
+	public static void main(String[] args) {
+		// implement this
+	}
+}
---
+++ Workshop5/Results.md
@@ -0,0 +1,4 @@
+# Copy your commands and output here, clearly labeled.
+
+# Write your answers to questions here, too.
+ 
