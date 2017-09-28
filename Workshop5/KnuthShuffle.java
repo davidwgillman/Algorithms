c6218e056680370ee2a73d26e2938cab0e891c21
Andrew Leonov <mr.andreleon@gmail.com>
2017-09-28 14:16:59 -0400

    Knuth Shuffle Progress

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -1,22 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.tools.JavaCompiler;
+
 /* Shuffles an array of objects */
 
-import edu.princeton.cs.algs4.Stopwatch;
-import edu.princeton.cs.algs4.StdRandom;
-
 public final class KnuthShuffle {
-
-    /**
-     * Rearranges the elements of the specified array in uniformly random order.
-     *
-     * @param  a the array to shuffle
-     */
-	public static void shuffle(Object[] a) {
-		// as a demo, this generates a random number from 0 to 9
-		int i = StdRandom.uniform(0,10); 
-		// implement this
-	}
-
 	public static void main(String[] args) {
-		// implement this
+		int[] ar1 = {1, 2, 3, 4};
+		int count = 0;
+		int[] ar_compare = new int[4];
+		
+		System.arraycopy(ar1, 0, ar_compare, 0, ar1.length);
+		
+		Stopwatch stpwtch = new Stopwatch();
+		
+		for (int i = 0; i < 2400; i++) {
+			shuffle(ar1);
+			if (Arrays.equals(ar1, ar_compare)) {
+				count++;
+			}
+			System.arraycopy(ar1, 0, ar_compare, 0, ar1.length);
+		}
+		
+		// print elapsed time
+		StdOut.println("Elapsed Time: " + stpwtch.elapsedTime());
+		StdOut.println("Arrays repeated: " + count + " times.");
+	}
+	
+	public static void shuffle(int[] a) {
+		// as a demo, this generates a random number from 0 to 9
+		for (int x = 0; x < a.length - 1; x++) {
+			int i = StdRandom.uniform(0,4); 
+			int swap;
+			swap = a[x];
+			a[x] = a[i];
+			a[i] = swap;
+		}
 	}
 }
--- Workshop5/Results.md
+++ Workshop5/Results.md
@@ -1,4 +1,42 @@
-# Copy your commands and output here, clearly labeled.
+// Andrew Leonov
 
-# Write your answers to questions here, too.
- 
+Workshop #5
+
+Data Size	Elapsed Time
+100,000		0.021 s
+200,000		0.036 s
+400,000		0.041 s
+800,000		0.059 s
+
+
+5. Run KnuthShuffle.java to see the results.
+
+Code: 
+
+public static void main(String[] args) {
+		int[] ar1 = {1, 2, 3, 4};
+		int count = 0;
+		int[] ar_compare = new int[4];
+		
+		System.arraycopy(ar1, 0, ar_compare, 0, ar1.length);
+		
+		Stopwatch stpwtch = new Stopwatch();
+		
+		for (int i = 0; i < 2400; i++) {
+			shuffle(ar1);
+			if (Arrays.equals(ar1, ar_compare)) {
+				count++;
+			}
+			System.arraycopy(ar1, 0, ar_compare, 0, ar1.length);
+		}
+		
+		// print elapsed time
+		StdOut.println("Elapsed Time: " + stpwtch.elapsedTime());
+		StdOut.println("Arrays repeated: " + count + " times.");
+	}
+
+
+Output:
+
+Elapsed Time: 0.004
+Arrays repeated: 138 times.
