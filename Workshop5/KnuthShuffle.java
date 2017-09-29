a99d1fadf9c8cb24956d447403aab57f9bc22820
6enevieve <genevieve@Genevieves-MacBook-Air.local>
2017-09-28 20:31:58 -0400

    Implemented KnuthShuffle class and updated Results

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -3,6 +3,7 @@
 import edu.princeton.cs.algs4.Stopwatch;
 import edu.princeton.cs.algs4.StdRandom;
 
+
 public final class KnuthShuffle {
 
     /**
@@ -10,13 +11,190 @@ public final class KnuthShuffle {
      *
      * @param  a the array to shuffle
      */
+    public static void swap(Object[] a, int i, int r){
+		if(i < a.length && r < a.length){
+			Object m = a[i];
+			Object n = a[r];
+			a[i] = n;
+			a[r] = m;
+		}
+		return;
+	}
+
+	public static Integer[] arrayBuilder(int n){
+    	Integer[] result = new Integer[n];
+    	for(int i = 0; i < n; i++){
+			result[i] = i;
+		}
+		return result;
+	}
+
 	public static void shuffle(Object[] a) {
 		// as a demo, this generates a random number from 0 to 9
-		int i = StdRandom.uniform(0,10); 
+		int l = StdRandom.uniform(0,10);
 		// implement this
+		for(int i = 1, r; i < a.length; i++){
+			r = StdRandom.uniform(0,i);
+			swap(a, i, r);
+		}
+		return;
+        
 	}
 
 	public static void main(String[] args) {
-		// implement this
+		Integer[] mainArray = {1,2,3,4};
+		KnuthShuffle shuffler = new KnuthShuffle();
+		int a1234=0, a1243=0, a1324=0, a1342=0, a1423=0, a1432=0, a2134=0, a2143=0, a2314=0, a2341=0, a2413=0, a2431=0;
+		int a3124=0, a3142=0, a3214=0, a3241=0, a3412=0, a3421=0, a4123=0, a4132=0, a4213=0, a4231=0, a4312=0, a4321=0;
+		for(int i = 0; i <= 2400; i++){
+			shuffler.shuffle(mainArray);
+
+			switch(mainArray[0]){
+				case 1:
+					switch(mainArray[1]){
+						case 2:
+							switch(mainArray[2]){
+								case 3:
+									//cool its 1234
+									a1234++;
+								case 4:
+									//cool its 1243
+									a1243++;
+							}
+						case 3:
+							switch(mainArray[2]){
+								case 2:
+									//cool its 1324
+									a1324++;
+								case 4:
+									//cool its 1342
+									a1342++;
+							}
+						case 4:
+							switch(mainArray[2]){
+								case 2:
+									//cool its 1423
+									a1423++;
+								case 3:
+									//cool its 1432
+									a1432++;
+							}
+					}
+				case 2:
+					switch(mainArray[1]){
+						case 1:
+							switch(mainArray[2]){
+								case 3:
+									//cool its 2134
+									a2134++;
+								case 4:
+									//cool its 2143
+									a2143++;
+							}
+						case 3:
+							switch(mainArray[2]){
+								case 1:
+									//cool its 2314
+									a2314++;
+								case 4:
+									//cool its 2341
+									a2341++;
+							}
+						case 4:
+							switch(mainArray[2]){
+								case 1:
+									//cool its 2413
+									a2413++;
+								case 2:
+									//cool its 2431
+									a2431++;
+							}
+					}
+				case 3:
+					switch(mainArray[1]){
+						case 1:
+							switch(mainArray[2]){
+								case 4:
+									//cool its 3142
+									a3142++;
+								case 2:
+									//cool its 3124
+									a3124++;
+							}
+						case 2:
+							switch(mainArray[2]){
+								case 1:
+									//cool its 3214
+									a3214++;
+								case 4:
+									//cool its 3241
+									a3241++;
+							}
+						case 4:
+							switch(mainArray[2]){
+								case 1:
+									//cool its 3412
+									a3412++;
+								case 2:
+									//cool its 3421
+									a3421++;
+							}
+					}
+				case 4:
+					switch(mainArray[1]){
+						case 1:
+							switch(mainArray[2]){
+								case 2:
+									//cool its 4123
+									a4123++;
+								case 3:
+									//cool its 4132
+									a4132++;
+							}
+						case 2:
+							switch(mainArray[2]){
+								case 1:
+									//cool its 4213
+									a4213++;
+								case 3:
+									//cool its 4231
+									a4231++;
+							}
+						case 3:
+							switch(mainArray[2]){
+								case 1:
+									//cool its 4312
+									a4312++;
+								case 2:
+									//cool its 4321
+									a4321++;
+							}
+					}
+			}
+		}
+		System.out.println(a1234 + " of permutation 1234");
+		System.out.println(a1243 + " of permutation 1243");
+		System.out.println(a1324 + " of permutation 1324");
+		System.out.println(a1342 + " of permutation 1342");
+		System.out.println(a1423 + " of permutation 1423");
+		System.out.println(a1432 + " of permutation 1432");
+		System.out.println(a2134 + " of permutation 2134");
+		System.out.println(a2143 + " of permutation 2143");
+		System.out.println(a2314 + " of permutation 2314");
+		System.out.println(a2341 + " of permutation 2341");
+		System.out.println(a2413 + " of permutation 2413");
+		System.out.println(a2431 + " of permutation 2431");
+		System.out.println(a3124 + " of permutation 3124");
+		System.out.println(a3142 + " of permutation 3142");
+		System.out.println(a3214 + " of permutation 3214");
+		System.out.println(a3241 + " of permutation 3241");
+		System.out.println(a3412 + " of permutation 3412");
+		System.out.println(a3421 + " of permutation 3421");
+		System.out.println(a4123 + " of permutation 4123");
+		System.out.println(a4132 + " of permutation 4132");
+		System.out.println(a4213 + " of permutation 4213");
+		System.out.println(a4231 + " of permutation 4231");
+		System.out.println(a4312 + " of permutation 4312");
+		System.out.println(a4321 + " of permutation 4321");
 	}
 }
--- Workshop5/Results.md
+++ Workshop5/Results.md
@@ -1,4 +1,48 @@
 # Copy your commands and output here, clearly labeled.
 
 # Write your answers to questions here, too.
- 
+# 3. Time your algorithm on input arrays of size 1 million, 2 million, 4 million, and 8 million and show the results in a table:
+
+Input Size | Time to Run | Ratio Between Previous
+--- | --- | ---
+1,000,000 | 0.224 | x
+2,000,000 | 0.302 | 1.35
+4,000,000 | 0.487 | 1.61
+8,000,000 | 1.119 | 2.3
+
+# 4. Estimate the running time in the form aN^b using the doubling method:
+
+Using the doubling rule, increasing input size until limit is found:
+
+Input Size | Time to Run | Ratio Between Previous
+--- | --- | ---
+10000 | 0.012 | x
+20000 | 0.006 | 0.5
+40000 | 0.006 | 1
+80000 | 0.012 | 2
+160000 | 0.093 | 7.75
+320000 | 0.101 | 1.09
+640000 | 0.282 | 2.79
+1280000 | 0.693 | 2.46
+2560000 | 1.066 | 1.54
+5120000 | 1.38 | 1.29
+10240000 | 5.967 | 4.32
+20480000 | 22.427 | 3.76
+40960000 | 38.464 | 1.72
+81920000 | 86.588 | 2.25
+
+
+Let's just say the limit we'll be approaching is 2?
+So if the limit we're approaching is 2, then b = 1
+What could a be? Basically we know that this function is aN^1, or aN. We can figure out roughly what a is by simply dividing for each term given our empirical evidence.
+
+For 81920000, 86.588 / 81920000 = 0.000001056982422, which is tiny!
+For 1280000, 0.693 / 1280000 = 0.00000054140625, which is also tiny.
+
+So the order of growth is ~0.0000005*N. This is basically linear time, which makes sense because each element is being shuffled once, so there is really only one major operation per element of the array.
+
+# 5. Write a program to shuffle the array [1,2,3,4] 2400 times, count how many times each shuffled version of the array appears, and print the results.
+
+
+
+
