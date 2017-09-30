1dd0cf05651da5433a84744b6d5a0edab2060a82
6enevieve <genevieve@Genevieves-MacBook-Air.local>
2017-09-30 15:52:09 -0400

    Fixed problem 5 code and completed problem 6

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -42,13 +42,64 @@ public final class KnuthShuffle {
 	}
 
 	public static void main(String[] args) {
-		Integer[] mainArray = {1,2,3,4};
+		Integer[] ma = {1,2,3,4};
 		KnuthShuffle shuffler = new KnuthShuffle();
 		int a1234=0, a1243=0, a1324=0, a1342=0, a1423=0, a1432=0, a2134=0, a2143=0, a2314=0, a2341=0, a2413=0, a2431=0;
 		int a3124=0, a3142=0, a3214=0, a3241=0, a3412=0, a3421=0, a4123=0, a4132=0, a4213=0, a4231=0, a4312=0, a4321=0;
-		for(int i = 0; i <= 2400; i++){
-			shuffler.shuffle(mainArray);
-
+		int m = 0;
+		for(int i = 0; i <= 2400; i++) {
+			shuffler.shuffle(ma);
+			if (ma[0] == 1 && ma[1] == 2 && ma[2] == 3 && ma[3] == 4) {
+				a1234++;
+			} else if (ma[0] == 1 && ma[1] == 2 && ma[2] == 4 && ma[3] == 3) {
+				a1243++;
+			} else if (ma[0] == 1 && ma[1] == 2 && ma[2] == 2 && ma[3] == 4) {
+				a1324++;
+			} else if (ma[0] == 1 && ma[1] == 3 && ma[2] == 4 && ma[3] == 2) {
+				a1342++;
+			} else if (ma[0] == 1 && ma[1] == 4 && ma[2] == 2 && ma[3] == 3) {
+				a1423++;
+			} else if (ma[0] == 1 && ma[1] == 4 && ma[2] == 2 && ma[3] == 3) {
+				a1432++;
+			} else if (ma[0] == 2 && ma[1] == 1 && ma[2] == 3 && ma[3] == 4) {
+				a2134++;
+			} else if (ma[0] == 2 && ma[1] == 1 && ma[2] == 4 && ma[3] == 3) {
+				a2143++;
+			} else if (ma[0] == 2 && ma[1] == 3 && ma[2] == 1 && ma[3] == 4) {
+				a2314++;
+			} else if (ma[0] == 2 && ma[1] == 3 && ma[2] == 4 && ma[3] == 1) {
+				a2341++;
+			} else if (ma[0] == 2 && ma[1] == 4 && ma[2] == 1 && ma[3] == 3) {
+				a2413++;
+			} else if (ma[0] == 2 && ma[1] == 4 && ma[2] == 3 && ma[3] == 1) {
+				a2431++;
+			} else if (ma[0] == 3 && ma[1] == 1 && ma[2] == 2 && ma[3] == 4) {
+				a3124++;
+			} else if (ma[0] == 3 && ma[1] == 1 && ma[2] == 4 && ma[3] == 2) {
+				a3142++;
+			} else if (ma[0] == 3 && ma[1] == 2 && ma[2] == 1 && ma[3] == 4) {
+				a3214++;
+			} else if (ma[0] == 3 && ma[1] == 2 && ma[2] == 4 && ma[3] == 1) {
+				a3241++;
+			} else if (ma[0] == 3 && ma[1] == 4 && ma[2] == 1 && ma[3] == 2) {
+				a3412++;
+			} else if (ma[0] == 3 && ma[1] == 4 && ma[2] == 2 && ma[3] == 1) {
+				a3421++;
+			} else if (ma[0] == 4 && ma[1] == 1 && ma[2] == 2 && ma[3] == 3) {
+				a4123++;
+			} else if (ma[0] == 4 && ma[1] == 1 && ma[2] == 3 && ma[3] == 2) {
+				a4132++;
+			} else if (ma[0] == 4 && ma[1] == 2 && ma[2] == 1 && ma[3] == 3) {
+				a4213++;
+			} else if (ma[0] == 4 && ma[1] == 2 && ma[2] == 3 && ma[3] == 1) {
+				a4231++;
+			} else if (ma[0] == 4 && ma[1] == 3 && ma[2] == 1 && ma[3] == 2) {
+				a4312++;
+			} else if (ma[0] == 4 && ma[1] == 3 && ma[2] == 2 && ma[3] == 1) {
+				a4321++;
+			}
+		}
+			/*
 			switch(mainArray[0]){
 				case 1:
 					switch(mainArray[1]){
@@ -57,27 +108,33 @@ public final class KnuthShuffle {
 								case 3:
 									//cool its 1234
 									a1234++;
+									m++;
 								case 4:
 									//cool its 1243
 									a1243++;
+									m++;
 							}
 						case 3:
 							switch(mainArray[2]){
 								case 2:
 									//cool its 1324
 									a1324++;
+									m++;
 								case 4:
 									//cool its 1342
 									a1342++;
+									m++;
 							}
 						case 4:
 							switch(mainArray[2]){
 								case 2:
 									//cool its 1423
 									a1423++;
+									m++;
 								case 3:
 									//cool its 1432
 									a1432++;
+									m++;
 							}
 					}
 				case 2:
@@ -87,27 +144,33 @@ public final class KnuthShuffle {
 								case 3:
 									//cool its 2134
 									a2134++;
+									m++;
 								case 4:
 									//cool its 2143
 									a2143++;
+									m++;
 							}
 						case 3:
 							switch(mainArray[2]){
 								case 1:
 									//cool its 2314
 									a2314++;
+									m++;
 								case 4:
 									//cool its 2341
 									a2341++;
+									m++;
 							}
 						case 4:
 							switch(mainArray[2]){
 								case 1:
 									//cool its 2413
 									a2413++;
+									m++;
 								case 2:
 									//cool its 2431
 									a2431++;
+									m++;
 							}
 					}
 				case 3:
@@ -117,27 +180,33 @@ public final class KnuthShuffle {
 								case 4:
 									//cool its 3142
 									a3142++;
+									m++;
 								case 2:
 									//cool its 3124
 									a3124++;
+									m++;
 							}
 						case 2:
 							switch(mainArray[2]){
 								case 1:
 									//cool its 3214
 									a3214++;
+									m++;
 								case 4:
 									//cool its 3241
 									a3241++;
+									m++;
 							}
 						case 4:
 							switch(mainArray[2]){
 								case 1:
 									//cool its 3412
 									a3412++;
+									m++;
 								case 2:
 									//cool its 3421
 									a3421++;
+									m++;
 							}
 					}
 				case 4:
@@ -147,31 +216,37 @@ public final class KnuthShuffle {
 								case 2:
 									//cool its 4123
 									a4123++;
+									m++;
 								case 3:
 									//cool its 4132
 									a4132++;
+									m++;
 							}
 						case 2:
 							switch(mainArray[2]){
 								case 1:
 									//cool its 4213
 									a4213++;
+									m++;
 								case 3:
 									//cool its 4231
 									a4231++;
+									m++;
 							}
 						case 3:
 							switch(mainArray[2]){
 								case 1:
 									//cool its 4312
 									a4312++;
+									m++;
 								case 2:
 									//cool its 4321
 									a4321++;
+									m++;
 							}
 					}
-			}
-		}
+			} */
+
 		System.out.println(a1234 + " of permutation 1234");
 		System.out.println(a1243 + " of permutation 1243");
 		System.out.println(a1324 + " of permutation 1324");
@@ -196,5 +271,6 @@ public final class KnuthShuffle {
 		System.out.println(a4231 + " of permutation 4231");
 		System.out.println(a4312 + " of permutation 4312");
 		System.out.println(a4321 + " of permutation 4321");
+		System.out.println("m is " + m);
 	}
 }
--- Workshop5/Results.md
+++ Workshop5/Results.md
@@ -10,7 +10,7 @@ Input Size | Time to Run | Ratio Between Previous
 4,000,000 | 0.487 | 1.61
 8,000,000 | 1.119 | 2.3
 
-# 4. Estimate the running time in the form aN^b using the doubling method:
+# 4. Estimate the running time in the form aN^b^ using the doubling method:
 
 Using the doubling rule, increasing input size until limit is found:
 
@@ -34,7 +34,7 @@ Input Size | Time to Run | Ratio Between Previous
 
 Let's just say the limit we'll be approaching is 2?
 So if the limit we're approaching is 2, then b = 1
-What could a be? Basically we know that this function is aN^1, or aN. We can figure out roughly what a is by simply dividing for each term given our empirical evidence.
+What could a be? Basically we know that this function is aN^1^, or aN. We can figure out roughly what a is by simply dividing for each term given our empirical evidence.
 
 For 81920000, 86.588 / 81920000 = 0.000001056982422, which is tiny!
 For 1280000, 0.693 / 1280000 = 0.00000054140625, which is also tiny.
@@ -42,6 +42,36 @@ For 1280000, 0.693 / 1280000 = 0.00000054140625, which is also tiny.
 So the order of growth is ~0.0000005*N. This is basically linear time, which makes sense because each element is being shuffled once, so there is really only one major operation per element of the array.
 
 # 5. Write a program to shuffle the array [1,2,3,4] 2400 times, count how many times each shuffled version of the array appears, and print the results.
+
+85 of permutation 1234
+80 of permutation 1243
+0 of permutation 1324
+101 of permutation 1342
+108 of permutation 1423
+0 of permutation 1432
+112 of permutation 2134
+95 of permutation 2143
+94 of permutation 2314
+101 of permutation 2341
+91 of permutation 2413
+94 of permutation 2431
+90 of permutation 3124
+77 of permutation 3142
+99 of permutation 3214
+104 of permutation 3241
+100 of permutation 3412
+123 of permutation 3421
+95 of permutation 4123
+115 of permutation 4132
+109 of permutation 4213
+90 of permutation 4231
+108 of permutation 4312
+105 of permutation 4321
+
+# 6. Explain why the invariant in the demo is true: after round i, the array is uniformly shuffled from index 0 to index i.
+For each round, a random number j is chosen between 0 and i. The element at index j is then swapped with the element at index i. This ensures that, for each new element that i refers to, there is an equal chance it will go anywhere in the array thus far from index 0 to index i. Further, any element from index i, after getting swapped into index j, has an equal chance of getting *re-swapped* into i in a later round, because the algorithm always chooses a random j between 0 and i. For example, say we are at round 13 in our program, so i = 13. j is chosen to be 5, so the element at index 5 is now at index 13, and the element at index 13 is now at index 5. 3 rounds later, when i = 16, every element that has been shuffled so far has a 1 in 15 chance of being chosen to be swapped. It just so happens that this time, j is chosen to be 5 again, so the element at index 5 (which was previously in index i when i was 13) is now put into index 16 and vice versa. The chance of this happening for any element between index 0 and i is equally likely each round, because the rounds are independent of each other. Therefore, the array will always be uniformly shuffled from 0 to i.
+
+
 
 
 
