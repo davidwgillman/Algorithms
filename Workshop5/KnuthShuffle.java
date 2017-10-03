5996eb4f8a9cea5297ea49147d0444d75d2d8c22
CharStone97 <charles.stone15@ncf.edu>
2017-10-03 17:34:44 -0400

    Shuffle implemented and questions answered

--- Workshop5/KnuthShuffle.java
+++ Workshop5/KnuthShuffle.java
@@ -2,6 +2,7 @@
 
 import edu.princeton.cs.algs4.Stopwatch;
 import edu.princeton.cs.algs4.StdRandom;
+import java.util.Arrays; 
 
 public final class KnuthShuffle {
 
@@ -10,13 +11,29 @@ public final class KnuthShuffle {
      *
      * @param  a the array to shuffle
      */
+    
 	public static void shuffle(Object[] a) {
-		// as a demo, this generates a random number from 0 to 9
-		int i = StdRandom.uniform(0,10); 
-		// implement this
-	}
+        for (int i = 0; i < a.length; i++) {
+            int random = StdRandom.uniform(0,a.length);
+            Object temp = a[random];
+            a[random] = a[i];
+            a[i] = temp;
+        }
+        //
+    }
 
-	public static void main(String[] args) {
-		// implement this
-	}
+    public static void main(String[] args) {
+        Object[] x = new Object[400000];
+        for(int i = 0; i < x.length; i++){
+            x[i] = i;
+        }
+        Stopwatch stopwatch = new Stopwatch();
+        long start = System.currentTimeMillis();
+        //System.out.println(Arrays.toString(x));
+        shuffle(x);
+        long elapsed = System.currentTimeMillis();
+        //System.out.println("Milliseconds elapsed: "+ (elapsed - start));
+        //System.out.println(Arrays.toString(x));
+        System.out.println("Milliseconds elapsed: "+ (elapsed - start));
+    }
 }
--- Workshop5/Results.md
+++ Workshop5/Results.md
@@ -1,4 +1,39 @@
-# Copy your commands and output here, clearly labeled.
+3. Times for Knuth Shuffle
 
-# Write your answers to questions here, too.
- 
+ N = 1,000,000 | time = 406 ms
+
+ N = 2,000,000 | time = 719 ms
+
+ N = 4,000,000 | time = 1,594 ms
+
+ N = 8,000,000 | time = 3,291 ms
+
+
+4. Estimated Runtime in a(N)^b
+
+	0.000000411375(N)^!
+
+5. Results of Shuffling [1,2,3,4] 2,400 times
+ Occurrences of {1, 2, 4, 3} : 95
+ Occurrences of {1, 3, 2, 4} : 90
+ Occurrences of {1, 3, 4, 2} : 112
+ Occurrences of {1, 4, 2, 3} : 103
+ Occurrences of {1, 4, 3, 2} : 80
+ Occurrences of {2, 1, 3, 4} : 93
+ Occurrences of {2, 1, 4, 3} : 147
+ Occurrences of {2, 3, 1, 4} : 138
+ Occurrences of {2, 3, 4, 1} : 118
+ Occurrences of {2, 4, 1, 3} : 119
+ Occurrences of {2, 4, 3, 1} : 118
+ Occurrences of {3, 1, 2, 4} : 95
+ Occurrences of {3, 1, 4, 2} : 92
+ Occurrences of {3, 2, 1, 4} : 89
+ Occurrences of {3, 2, 4, 1} : 113
+ Occurrences of {3, 4, 1, 2} : 89
+ Occurrences of {3, 4, 2, 1} : 95 
+ Occurrences of {4, 1, 2, 3} : 78
+ Occurrences of {4, 1, 3, 2} : 87
+ Occurrences of {4, 2, 1, 3} : 89
+ Occurrences of {4, 2, 3, 1} : 77
+ Occurrences of {4, 3, 1, 2} : 100
+ Occurrences of {4, 3, 2, 1} : 96
