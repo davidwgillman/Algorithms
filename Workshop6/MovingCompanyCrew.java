fa3b925255039dc89222daa80fae306a925e5d30
Andrew Leonov <mr.andreleon@gmail.com>
2017-10-08 01:01:27 -0400

    Workshop 6 - Code

--- Workshop6/MovingCompanyCrew.java
+++ Workshop6/MovingCompanyCrew.java
@@ -1,9 +1,21 @@
 /* MovingCompanyCrew: contains a list of jobs in a day's work and an hoursOfWork() method
 */
 
-public class MovingCompanyCrew implements Comparable<MovieScore> {
-	double[] jobs = null;
+public class MovingCompanyCrew {
+	double[] jobs = new double[10];
+	int njobs = 0;
 
 	public double hoursOfWork() {
-		
+		double acc = 0;
+		for (double job : jobs) {
+			acc += job;
+		}
+		return acc;
 	}
+	
+	public void assignJob(double job) {
+		jobs[njobs] = job;
+		njobs++;
+	}
+	
+}
--- Workshop6/MovingCompanyDispatcher.java
+++ Workshop6/MovingCompanyDispatcher.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /* Randomly generates a day's worth of jobs for H households using V van crews
    H and V may be passed through a constructor or a newDay() method.
 
@@ -15,38 +17,106 @@
  	private int nCrews = 0;
  	private int nHouseholds = 0;
  	private double[] jobs = null;
+ 	private int jobpos = 0;
 
  	private MovingCompanyCrew[] crews = null;
 
- 	public MovingCompanyDispatcher()
-
- 	public newDay(int C, int H) {
+ 	public void newDay(int C, int H) {
+ 		nCrews = C;
+ 		nHouseholds = H;
+ 		jobpos = 0;
+ 		makeCrews();
+ 		makeJobs();
  	}
 
  	public double getLowerBound() {
- 		// lower bound on numnber of hours worked by last crew to get off work
- 		return ; 
+ 		// lower bound on number of hours worked by last crew to get off work
+ 		double jobTimes = 0;
+ 		for (double job : jobs) {
+ 			jobTimes += job;
+ 		}
+ 		jobTimes = (Math.round((jobTimes/crews.length) * 100.0) / 100.0);
+ 		return jobTimes; 
+ 	}
+ 	
+ 	public void makeCrews() {
+ 		crews = new MovingCompanyCrew[nCrews];
+ 		for (int i = 0; i < crews.length; i++) {
+ 			crews[i] = new MovingCompanyCrew();
+ 		}
  	}
 
  	public void makeJobs() {
- 		// generate random jobs
+ 		jobs = new double[nHouseholds];
+ 		Random rand = new Random();
+ 		for (int i = 0; i < jobs.length; i++) {
+ 			jobs[i] = (Math.round(rand.nextDouble() * 1000.0) / 100.0);
+ 		}
+ 		
+ 		// order jobs longest to shortest 
+ 	 		for (int i = 0; i < jobs.length; i++) {
+ 	 			int max = 0;
+ 	 			for (int j = i; j < jobs.length; j++) {
+ 	 				if (jobs[j] > jobs[max]) {
+ 	 					max = j;
+ 	 				}
+ 	 			double temp = jobs[i];
+ 	 			jobs[i] = jobs[max];
+ 	 			jobs[max] = temp;
+ 	 			}
+ 	 		}
  	}
 
- 	public void assignJobs() {
- 		// order jobs longest to shortest 
-
- 		// assign each job in order to first available crew 
+ 	public void assignJobs() { 		
+ 		// assign each job in order to first available crew
+ 		for (MovingCompanyCrew crew : crews) {
+ 			if (jobpos < jobs.length) {
+	 			crew.assignJob(jobs[jobpos]);
+	 			jobpos++;
+ 			}
+ 		}
+ 		
+ 		orderCrews();
+ 		
+ 		if (jobpos < jobs.length) {
+ 			assignJobs();
+ 		}
+ 	}
+ 	
+ 	private void orderCrews() {
+ 		for (int i = 0; i < crews.length; i++) {
+ 			int min = 0;
+ 			for (int j = i; j < crews.length; j++) {
+ 				if (crews[j].hoursOfWork() < crews[min].hoursOfWork()) {
+ 					min = j;
+ 				}
+ 			MovingCompanyCrew temp = crews[i];
+ 			crews[i] = crews[min];
+ 			crews[min] = temp;
+ 			}
+ 		}
  	}
 
  	// this method lets the client figure out how good a job the Dispatcher did
  	// Alternatively, you can let the Dispatcher do its own self-evaluation 
  	// and expose the stats in different method
- 	public MovingCompanyCrew[] getCrews() {
-
+ 	public void printCrews() {
+ 		double lowerBound = getLowerBound(); 
+ 		for (int i = 0; i < crews.length; i++ ) {
+ 			double crewHours = crews[i].hoursOfWork();
+ 			System.out.println("\nCrew " + (i + 1) + ": " + crewHours);
+ 			if (crewHours > lowerBound) {
+ 				double excessPerc = (Math.round((100 - ((lowerBound/crewHours) * 100.0)) * 100.0) / 100.0);
+ 				System.out.println("Excess hours: " + (Math.round((crewHours - lowerBound) * 100.0) / 100.0) + " hrs / " + excessPerc + "%");
+ 			}
+ 		}
  	}
 
  	public static void main(String[] args) {
-
+ 		MovingCompanyDispatcher mcd = new MovingCompanyDispatcher();
+ 		mcd.newDay(5, 10);
+ 		mcd.assignJobs();
+ 		mcd.printCrews();
  	}
 
  }
