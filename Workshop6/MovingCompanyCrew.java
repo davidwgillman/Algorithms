6ec2b242e597d8a88ebc5dedcef01de40673b0e4
dianatarazi <diana.tarazi15@ncf.edu>
2017-10-08 21:48:55 -0400

    Workshop 6 code

--- Workshop6/MovingCompanyCrew.java
+++ Workshop6/MovingCompanyCrew.java
@@ -1,9 +1,32 @@
 /* MovingCompanyCrew: contains a list of jobs in a day's work and an hoursOfWork() method
 */
 
-public class MovingCompanyCrew implements Comparable<MovieScore> {
+public class MovingCompanyCrew implements Comparable<MovingCompanyCrew> {
 	double[] jobs = null;
 
-	public double hoursOfWork() {
-		
+	public MovingCompanyCrew(int jobsN) {
+		jobs = new double[jobsN];
+		for (int i = 0; i < jobsN; i++){
+			jobs[i] = 0;
+
+		}
 	}
+
+	public double hoursOfWork() {
+		//adds up hours of work for each crew
+		double sum = 0;
+		for (double d : jobs) {
+			sum += d;
+		}
+		return sum;
+	}
+
+	public int compareTo(MovingCompanyCrew m) {
+		double dif = this.hoursOfWork()-m.hoursOfWork();
+		if (dif<0)
+			return -1;
+		else if (dif>0)
+			return 1;
+		return 0;
+	}
+
--- Workshop6/MovingCompanyDispatcher.java
+++ Workshop6/MovingCompanyDispatcher.java
@@ -10,6 +10,9 @@
    */
 
 // imports
+ //import edu.princeton.cs.algs4.StdRandom;
+ //import edu.princeton.cs.algs4.insertion;
+ import java.util.*;
 
  public class MovingCompanyDispatcher {
  	private int nCrews = 0;
@@ -18,34 +21,96 @@
 
  	private MovingCompanyCrew[] crews = null;
 
- 	public MovingCompanyDispatcher()
+ 	public MovingCompanyDispatcher() {
 
- 	public newDay(int C, int H) {
+ 	}
+
+ 	public void newDay(int C, double[] J) {
+ 		int H = J.length;
+ 		nCrews = C;
+ 		crews = new MovingCompanyCrew[nCrews];
+		nHouseholds = H;
+ 		jobs = new double[nHouseholds];
+ 		makeCrews();
+ 		jobs = J;	
  	}
 
  	public double getLowerBound() {
  		// lower bound on numnber of hours worked by last crew to get off work
- 		return ; 
+ 		double sum = 0;
+ 		for (int i=0; i < jobs.length; i++) {
+ 			sum += jobs[i];
+ 		}
+ 		double avg = sum/nCrews;
+ 		return avg; 
+ 	}
+
+ 	public void makeCrews() {
+ 		for (int i = 0; i < nCrews; i++){
+ 			crews[i] = new MovingCompanyCrew(nHouseholds);
+ 		}
+
+
  	}
 
  	public void makeJobs() {
  		// generate random jobs
+ 		double rndm = Math.random()*7+1;
+ 		for (int i = 0; i < nHouseholds; i++) {
+ 			jobs[i] = rndm;
+ 		}
+
+ 	}
+
+ 	public MovingCompanyCrew leastNumOfWork() {
+ 		int minInd = 0;
+ 		MovingCompanyCrew min = crews[0];
+ 		for (int i = 1; i < crews.length; i++) {
+ 			if (crews[i].compareTo(min) < 0) {
+ 			minInd = i;
+ 			min = crews[i];
+ 			}
+ 		}
+ 		return min;
+ 	}
+
+
+ 	public void addJobs(double job){
+ 		int i = 0;
+ 		while (jobs[i]>0.0) {
+ 			i++;
+ 		}
+ 		jobs[i] = job;
  	}
 
  	public void assignJobs() {
  		// order jobs longest to shortest 
-
- 		// assign each job in order to first available crew 
+ 		Arrays.sort(jobs);
+		// assign each job in order to first available crew
+		for (int i = 0; i < jobs.length; i--) {
+			MovingCompanyCrew min = leastNumOfWork();
+			min.addJobs(jobs[i]);
+		} 
  	}
 
  	// this method lets the client figure out how good a job the Dispatcher did
  	// Alternatively, you can let the Dispatcher do its own self-evaluation 
  	// and expose the stats in different method
  	public MovingCompanyCrew[] getCrews() {
-
+ 		for (int i=0; i<nCrews;i++) {
+ 			System.out.println(crews[i]);
+ 		}
+ 		double lowerBound = getLowerBound();
+ 		System.out.println("Lower Bound: " + lowerBound + "hours");
  	}
 
  	public static void main(String[] args) {
+ 		MovingCompanyDispatcher mcp = new MovingCompanyDispatcher();
+ 		double[] j = new double[] {9,8,7,6,5,4,3,2};
+ 		mcp.newDay(2,j);
+ 		mcp.makeJobs()
+ 		mcp.assignJobs();
+ 		mcp.getCrews();
 
  	}
 
