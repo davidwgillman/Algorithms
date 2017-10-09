5d71dfcc82801e8ed8fb9b615c2e0c5651d7ffda
Sawyer <sawyer.welden@gmail.com>
2017-10-08 22:03:52 -0400

    finished

--- Workshop6/MovingCompanyCrew.java
+++ Workshop6/MovingCompanyCrew.java
@@ -1,14 +1,31 @@
 /* MovingCompanyCrew: contains a list of jobs in a day's work and an hoursOfWork() method
 */
 
-public class MovingCompanyCrew implements Comparable {
-	double[] jobs = null;
+import java.util.ArrayList;
+
+public class MovingCompanyCrew implements Comparable<MovingCompanyCrew> {
+	//double[] jobs = null;
+	ArrayList<Double> jobList = new ArrayList<Double>();
 
 	public double hoursOfWork() {
+		//System.out.println("we got here");
+		double sum = 0;
+		for (int i = 0; i < jobList.size(); i++){
+			sum += jobList.get(i);
+		}
+		return sum;
+	}
+
+	public int compareTo(MovingCompanyCrew o) {
+		MovingCompanyCrew b = (MovingCompanyCrew) o;
+		double me = hoursOfWork();
+		double them = b.hoursOfWork();
+		if (them > me) { return -1; } else 
+		if (me > them) { return 1; } else
 		return 0;
 	}
 
-	public int compareTo(Object o) {
-		return 0;
+	public void assignJob(double time) {
+		jobList.add(time);
 	}
 }
--- Workshop6/MovingCompanyDispatcher.java
+++ Workshop6/MovingCompanyDispatcher.java
@@ -10,50 +10,118 @@
    */
 
 // imports
-   import java.util.Random;
+   import java.util.*;
 
  public class MovingCompanyDispatcher {
- 	private int nCrews = 0;
- 	private int nHouseholds = 0;
- 	private double[] jobs = null;
+ 	private int nCrews = 2;
+ 	private int nHouseholds = 5;
+ 	private double[] jobs = new double[nHouseholds];
+ 	// make this an arraylist
 
- 	private MovingCompanyCrew[] crews = null;
+ 	private MovingCompanyCrew[] crews = new MovingCompanyCrew[nCrews];
 
- 	//public MovingCompanyDispatcher();
+ 	public MovingCompanyDispatcher(){
+ 		Random r = new Random();
+ 		//nHouseholds = 1 + 20 * r.nextInt();
+ 	}
+
+ 	public double[] getJobs() {
+ 		return jobs;
+ 	}
 
  	public void newDay(int C, int H) {
+ 		nCrews = C;
+ 		nHouseholds = H;
+ 		for (int i = 0; i < nCrews; i++) {
+ 			crews[i] = new MovingCompanyCrew();
+ 		}
  	}
 
  	public double getLowerBound() {
- 		// lower bound on numnber of hours worked by last crew to get off work
- 		return 0; 
+ 		double sum = 0;
+ 		for (int i = 0; i < jobs.length; i++) {
+ 			sum += jobs[i];
+ 		}
+ 		return sum /= crews.length;
  	}
 
  	public void makeJobs() {
  		// generate random jobs
  		Random r = new Random();
  		
- 		int num_jobs = 10 * r.nextInt();
- 		for (int i = 0; i < num_jobs; i++) {
+ 		for (int i = 0; i < nHouseholds; i++) {
  			double value = 1 + (5 * r.nextDouble());
+ 			jobs[i] = value;
  		}
  	}
 
  	public void assignJobs() {
  		// order jobs longest to shortest 
 
- 		// assign each job in order to first available crew 
+ 		// sort decending
+ 		Arrays.sort(jobs);
+
+ 		// reverse
+ 		for(int i = 0; i < jobs.length / 2; i++)
+		{
+    		double temp = jobs[i];
+    		jobs[i] = jobs[jobs.length - i - 1];
+    		jobs[jobs.length - i - 1] = temp;
+		}
+
+ 		// assign each job in order to first available crew
+
+ 		
+ 		for (int i = 0; i < nHouseholds; i++) {
+ 			// sort crews
+ 			sortCrews();
+ 			// assign job to lowest crew
+ 			crews[0].assignJob(jobs[i]);
+ 
+ 		}
+
+
  	}
 
  	// this method lets the client figure out how good a job the Dispatcher did
  	// Alternatively, you can let the Dispatcher do its own self-evaluation 
  	// and expose the stats in different method
+ 			// I chose to use this as my getter, evaluatePerfomance() checks how well it did 
  	public MovingCompanyCrew[] getCrews() {
- 		return null;
+ 		return crews;
+ 	}
+
+ 	public double evaluatePerfomance() {
+ 		sortCrews();
+ 		double longest = crews[crews.length -1].hoursOfWork();
+ 		return 100 * (longest - getLowerBound()) / getLowerBound();
+
+ 	}
+
+ 	// made a seperate method bc i kept changing data types
+ 	public void sortCrews() {
+ 		Arrays.sort(crews);
  	}
 
  	public static void main(String[] args) {
+ 		MovingCompanyDispatcher MCD = new MovingCompanyDispatcher();
+ 		MCD.newDay(2,5);
+ 		MCD.makeJobs();
+ 		MCD.assignJobs();
 
+ 		double jobs[] = MCD.getJobs();
+ 		for (int i = 0; i < jobs.length; i++) {
+ 			//System.out.println(jobs[i]);
+ 		}
+
+ 		MovingCompanyCrew[] crews = MCD.getCrews();
+ 		System.out.println("\nHours of work:");
+ 		for (int i = 0; i < crews.length; i ++) {
+ 			System.out.println(crews[i].hoursOfWork());
+ 		}
+
+ 		System.out.printf("\nTheoretical Optimum: %f\n",MCD.getLowerBound());
+ 		System.out.printf("\nExcess: %.2f%%\n",MCD.evaluatePerfomance());
  	}
 
  }
