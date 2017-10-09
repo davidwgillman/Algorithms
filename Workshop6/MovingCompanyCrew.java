1f2d779dc671ddcd11637e2b7900abd129a0dc15
Parankush <parankush@me.com>
2017-10-08 20:11:16 -0400

    Bug fixes.

--- Workshop6/MovingCompanyCrew.java
+++ Workshop6/MovingCompanyCrew.java
@@ -1,33 +1,61 @@
+import java.util.*;
 /* MovingCompanyCrew: contains a list of jobs in a day's work and an hoursOfWork() method
 */
 
 public class MovingCompanyCrew implements Comparable<MovingCompanyCrew> {
+                
+    //double[] jobs = null;
     
-    double[] jobs = null;
-    
-    double hours = 0;
-    
-    
+    ArrayList<Double> jobs = null;
     
     public MovingCompanyCrew() {
+        
+        jobs = new ArrayList<Double>();
 
     }
     
     
     public void setHours(double x) {
-       hours += x;
+        
+       jobs.add(x); 
+       
     }
     
     
+    //go through the hours of the jobs the crew has and return all the hours
     public double hoursOfWork() {
-        return hours;
+        
+        double hoursWorked = 0.0;
+        
+        for (int j = 0; j < jobs.size(); j++) {
+            hoursWorked += jobs.get(j);
+        }
+        
+        return hoursWorked;
     }
     
     
     public int compareTo(MovingCompanyCrew a) {
-       
-        return 0;
-     }
+        
+        //this is used to find the lowest hour working crew member
+        double difference = this.hoursOfWork() - a.hoursOfWork();
+        
+        if (difference <= 0) { //if this one is same or higher in value as a, then return -1
+            return -1;
+        }
+        else {  //if this crew member has a lower value, then return 1.
+            return 1;
+        }
+        
+    }
     
     
 }
+
+
+
+
+
+
+
+
--- Workshop6/MovingCompanyDispatcher.java
+++ Workshop6/MovingCompanyDispatcher.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 import edu.princeton.cs.algs4.Insertion;
-
+import java.util.*;
 /* Randomly generates a day's worth of jobs for H households using V van crews
    H and V may be passed through a constructor or a newDay() method.
    [Suggestion: create an array of MovingCompanyCrew objects;
                 a MovingCompanyCrew has a list of jobs and an hoursOfWork() method]
    Orders jobs from longest to shortest and assigns jobs in order to the first available crew.
    Prints the assignment and hours worked by the last crew to get off work.
-   */
+ */
 
 // imports
 
@@ -15,32 +15,98 @@ import edu.princeton.cs.algs4.Insertion;
     
     Random random = new Random();
      
-    private int nCrews = 0;
-    private int nHouseholds = 0;
+    int nCrews = 0;
+    int nHouseholds = 0;
     
     
-    private Double[] jobs = null;
+    Double[] jobs = null;
     
+    MovingCompanyCrew[] crews= null;
     
 
-    private MovingCompanyCrew[] crews = null;
+    
+    double lowerBoundNumber = 0;
+    double higherBoundNumber = 0;
+    
+    /*
+    public MovingCompanyDispatcher() {
 
-    //public MovingCompanyDispatcher();
+    }
+    */
 
     public void newDay(int C, int H) {
         nCrews = C;
         nHouseholds = H;
         
-        //assign length of jobs as number of households.
-        jobs = new Double[H];
-        
         //now assign c as number of crews
-        crews = new MovingCompanyCrew[C];
+        crews = new MovingCompanyCrew[nCrews];  
+        
+        
+        //assign length of jobs as number of households.
+        jobs = new Double[nHouseholds];
+        
     }
 
     public double getLowerBound() {
         // lower bound on numnber of hours worked by last crew to get off work
-        return 0; 
+        
+        //hours worked for lowest crew
+        lowerBoundNumber = 0.0;
+        
+        //lowest working crew member
+        MovingCompanyCrew lowestWorkingCrew = crews[0];
+        
+        
+        //go through the list of crews...
+        for(int a = 0; a < crews.length; a++) {
+        
+            //if theeres a new low one, set it as officla crew.
+            
+            if(crews[a].compareTo(lowestWorkingCrew) < 0) {
+                lowestWorkingCrew = crews[a];
+            }
+            
+            if (crews[a].hoursOfWork() < lowerBoundNumber) {
+                lowerBoundNumber = crews[a].hoursOfWork();
+                
+            }
+            
+        }
+        
+        return lowerBoundNumber; 
+    }
+    
+    public double getHigherBound() {
+        // higher  bound on numnber of hours worked by last crew to get off work
+        
+        higherBoundNumber = 0.0;
+        
+        for(int a = 0; a < crews.length; a++) {
+        
+            if (a == 0) {
+                higherBoundNumber = crews[a].hoursOfWork();
+                
+            }
+            
+            if (crews[a].hoursOfWork() > higherBoundNumber) {
+                higherBoundNumber = crews[a].hoursOfWork();
+                
+            }
+            
+        }
+        
+        return higherBoundNumber; 
+    }
+    
+    
+    public double getExcessHoursAsPercentageOfLowerBound() {
+        
+        double difference = higherBoundNumber - lowerBoundNumber;
+    
+        double percent = (difference/lowerBoundNumber) * 100;
+        
+        return percent;
+    
     }
 
     public void makeJobs() {
@@ -55,12 +121,22 @@ import edu.princeton.cs.algs4.Insertion;
         }
 
     }
-
+    
+    public void makeCrews() {
+    
+        //create crews
+        
+        for(int j = 0; j < crews.length; j++) {
+            crews[j] = new MovingCompanyCrew();   
+        }
+    
+    }
+    
     public void assignJobs() {
         // order jobs longest to shortest 
         Insertion.sort(jobs);
-        
-        //assign each job in order to first available crew 
+
+        //if there are more jobs than crewmembers...
         if (jobs.length >= crews.length) {
             
             int numberOfLoops = jobs.length/crews.length;
@@ -74,10 +150,11 @@ import edu.princeton.cs.algs4.Insertion;
             for(int x = numberOfLoops; x > 0; x--) {
                 
                 //we loop through all the crew member
-                for (int a = 0; a < crews.length; a++) {
+                for (int y = 0; y < crews.length; y++) {
                     //set jobs to each one
-                    crews[a].setHours(jobs[jobIndex]);
+                    crews[y].setHours(jobs[jobIndex]);
                     
+
                     //this job counter is used for the loop
                     jobIndex++;
                     
@@ -89,39 +166,55 @@ import edu.princeton.cs.algs4.Insertion;
             for (int b = 0; b < indexesInLastLoop; b++) {
                 //set jobs to each one
                 crews[b].setHours(jobs[jobIndex]);
+                
                     
                 //this job counter is used for the loop
                 jobIndex++;
-                    
+                
             }
-            
         }
-        
         
         
         if (crews.length > jobs.length) {
             for (int a = 0; a < jobs.length; a++) {
                 crews[a].setHours(jobs[a]);
+
             }
         }
         
-        
-  
-      
     }
 
     // this method lets the client figure out how good a job the Dispatcher did
     // Alternatively, you can let the Dispatcher do its own self-evaluation 
     // and expose the stats in different method
+    
+    
     public MovingCompanyCrew[] getCrews() {
-        
-        
         return crews;
-
     }
 
+    
     public static void main(String[] args) {
-
+        
+        MovingCompanyDispatcher mcd = new MovingCompanyDispatcher();
+        
+        
+        mcd.newDay(200,100);
+        mcd.makeJobs();
+        mcd.makeCrews();
+        mcd.assignJobs();
+        
+        
+        double low = mcd.getLowerBound();
+        double high = mcd.getHigherBound();
+        
+        double percentage = mcd.getExcessHoursAsPercentageOfLowerBound();
+        
+        System.out.println("Low: " + low);
+        System.out.println("High: " + high);
+        System.out.println("%: " + percentage);
+        
+        
     }
 
  }
